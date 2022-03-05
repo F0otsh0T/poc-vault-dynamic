f33d7765d5ba485f36f9ff9e7f6e0b1faa8a0872
F0otsh0T <jychen1@hotmail.com>
2022-03-04 23:15:14 -0800

    [F0otsh0T 20220304] Vault

---
+++ pki/tutorial/client/env.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -o xtrace
+
+#export VAULT_SKIP_VERIFY=true
+#unset VAULT_SKIP_VERIFY
+#export VAULT_ADDR='https://127.0.0.1:8200'
+#export VAULT_ADDR='https://:8200'
+#unset VAULT_ADDR
+#export VAULT_FORMAT=json
+#unset VAULT_FORMAT
+#export VAULT_TOKEN=$(pass vault/pki_test)
+#unset VAULT_TOKEN
