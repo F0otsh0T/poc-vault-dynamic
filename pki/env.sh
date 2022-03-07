f9ee8b828fdf3e7d7f2a6fd0af8f8ed7424eda8a
F0otsh0T <jychen1@hotmail.com>
2022-03-06 20:25:46 -0800

    [F0otsh0T 20220306] Vault

--- pki/env.sh
+++ pki/env.sh
@@ -4,9 +4,13 @@ set -o xtrace
 
 #export VAULT_SKIP_VERIFY=true
 #unset VAULT_SKIP_VERIFY
+#export VAULT_TOKEN=''
+#unset VAULT_TOKEN
 #export VAULT_ADDR='https://:8200'
-#export VAULT_ADDR='https://127.0.0.1:8200'
+#export VAULT_ADDR='http://0.0.0.0:8200'
+#export VAULT_ADDR='https://0.0.0.0:8200'
 #export VAULT_ADDR='http://127.0.0.1:8200'
+#export VAULT_ADDR='https://127.0.0.1:8200'
 #export VAULT_ADDR='https://127.0.0.1:8201'
 #export VAULT_ADDR='https://:8200'
 #unset VAULT_ADDR
